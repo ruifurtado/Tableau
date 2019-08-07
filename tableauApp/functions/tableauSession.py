df6ae6ae4504f1a1038e09a046f364f3f210b725
Rui Furtado <ruifurtado176379@gmail.com>
2019-08-07 15:48:48 +0100

    switch changed

--- tableauApp/functions/tableauSession.py
+++ tableauApp/functions/tableauSession.py
@@ -6,11 +6,28 @@ import sys
 
 class TableauSession():
 
-    def __init__(self, serverUrl, username, password, site):
-        self.serverUrl = serverUrl
-        self.username = username
-        self.password = password
-        self.site = site
+    def __init__(self):
+        self.connected = 0 
+
+    def initSession(self):
+        configPath='../myconfig.ini'
+        if os.path.isfile(configPath):
+            answer=input("\nConfiguration file available. Do you want to use it (Yes/No)?: ")
+            if answer=='Yes':
+                config = configparser.ConfigParser()
+                config.read(configPath)
+                self.serverUrl = config['SESSION']['server']
+                self.username = config['SESSION']['username']
+                self.password = config['SESSION']['password']
+                self.site = config['SESSION']['site']
+            if answer=='No':
+                print("\n-> Input user parameters\n")
+                self.serverUrl = input("Server link: ")
+                self.username = input("Username: ")
+                self.password = getpass.getpass("Password: ")
+                self.site = input("Site: ")
+        return self
+
 
     def connectToServer(self):
         try:
@@ -28,26 +45,30 @@ class TableauSession():
             print("\tUsername: {}".format(self.username))
             print("\tSite name: {}".format(self.site))
             print("\n----------------------------------------------------------\n")
-            return 1
+            self.connected = 1
+            return self
         except: 
-            print("\nInvalid login information!!!")
+            print("\nInvalid login information!!!") # Create limited number of tries
             print("Error: {}".format(sys.exc_info()))
             return ""
     
     def disconnectFromServer(self):
         self.serverConnection.auth.sign_out()
         print("Disconnecting from Tableau Server")
+        return ""
     
     def listSites(self):
         self.sites=self.serverConnection.sites.get()[0]
         for i,s in enumerate(self.sites):
             print("{}: {}\n\tURL: {}\n\tUser quota: {}\n\tStorage quota: {}\n\tState: {}\n".format(i,s.name,s.content_url,s.user_quota,s.storage_quota,s.state))
+        return ""
     
     def listProjects(self):
         self.projects=self.serverConnection.projects.get()[0]
         for i,p in enumerate(self.projects):
             print("{}: {}\n\tDescription: {}\n\tId: {}\n\tParent id: {}\n\tPermissions: {}\n".format(i,p.name,p.description,p.id,p.parent_id,p.content_permissions))
-    
+        return ""        
+            
     def listWorkbooks(self):
         self.workbooks = self.serverConnection.workbooks.get()[0]
         self.workbooks = [(w,w.project_name) for w in self.workbooks]
@@ -63,6 +84,7 @@ class TableauSession():
                     self.serverConnection.workbooks.populate_connections(w[0])
                     connections=[connection.datasource_name for connection in w[0].connections]
                     print("\t-{}) {} \n\t\tOwner: {} \n\t\tConnections: {}".format(counter,w[0].name,w[0].owner_id,connections))
+        return ""
     
     def listDataSources(self):
         self.dataSources = self.serverConnection.datasources.get()[0]
--- tableauApp/main.py
+++ tableauApp/main.py
@@ -4,40 +4,25 @@ import getpass
 import configparser
 import argparse
 
-configPath='../myconfig.ini'
-
-def initSession():
-    serverUrl=""
-    username=""
-    password=""
-    site=""
-    if os.path.isfile(configPath):
-        answer=input("\nConfiguration file available. Do you want to use it (Yes/No)?: ")
-        if answer=='Yes':
-            config = configparser.ConfigParser()
-            config.read(configPath)
-            serverUrl = config['SESSION']['server']
-            username = config['SESSION']['username']
-            password = config['SESSION']['password']
-            site = config['SESSION']['site']
-        if answer=='No':
-            print("\n-> Input user parameters\n")
-            serverUrl = input("Server link: ")
-            username = input("Username: ")
-            password = getpass.getpass("Password: ")
-            site = input("Site: ")
-    session=TableauSession(serverUrl,username,password,site)
+def initializeSession():
+    session=TableauSession()
+    while session.connected==0:
+        session=session.initSession()
+        session=session.connectToServer()
     return session
 
 def actionToTake(argument,session):
-    switcher = {
-        1:listSites(session),
-        2:listProjects(session),
-        3:listWorkbooks(session),
-        4:listDataSources(session),
-        5:exit(session)
-    }
-    func=switcher.get(int(argument),lambda:'Invalid input')  # Get the function from switcher dictionary
+    func=""
+    if int(argument)==1:
+        func=listSites(session)
+    elif int(argument)==2:
+        func=listProjects(session)
+    elif int(argument)==3:
+        func=listWorkbooks(session)
+    elif int(argument)==4:
+        func=listDataSources(session)
+    elif int(argument)==5:
+        func=exit(session)
     return func
 
 def listSites(session):
@@ -51,6 +36,7 @@ def listProjects(session):
 def listWorkbooks(session):
     session.listWorkbooks()
     return ""
+    
 def listDataSources(session):
     session.listDataSources()
     return ""
@@ -61,17 +47,14 @@ def exit(session):
 
 def main():
     print("\n------------------WELCOME TO TABLEAU APP------------------")
-    connection=""
-    while connection=="":
-        session=initSession()
-        connection=session.connectToServer()
-    while True:
+    session = initializeSession()
+    result=""
+    while result!="exit":
+        action = 0
+        result = 0
         print("1: List sites\n2: List projects\n3: List workbooks\n4: List data sources\n5: Exit\n")
         action = input("Action to take: ") 
-        print("")
         result=actionToTake(action,session)
-        if result=="exit":
-            break
 
 if __name__ == "__main__":
     main()    
